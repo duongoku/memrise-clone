8c9604602fc4b1e09c7e8e21236739a23f4d6766
duongoku <nguyenvubinhduong@gmail.com>
2022-09-16 13:47:11 +0700

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # random-flutter
 A flutter app for class INT3120 40
+
+## Members
+- 19020060 - Nguyễn Vũ Bình Dương
+- 19020095 - Nguyễn Hùng Dũng
+
+## Releases
+- Checkout [releases](https://github.com/duongoku/random-flutter/releases)
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(GameWidget(game: MyGame()));
+}
+
+class MyGame extends FlameGame with TapDetector {
+  TextComponent text = TextComponent();
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+
+    text
+      ..text = "Hello World\nWe're group 2 (Dương and _____)\nThis app is built with Flame"
+      ..y = 50
+      ..x = 50
+      ..scale = Vector2(0.75, 0.75);
+
+    add(text);
+  }
+}
