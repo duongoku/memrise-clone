68acb494c75eb8736fc4a3dc1616bd4d029e4704
emptywhite1 <emptywhite2@gmail.com>
2022-09-29 15:58:21 +0700

    add intro screen

---
+++ lib/Screen/GettingStartedScreen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class GettingStarted extends StatelessWidget {
+  const GettingStarted({super.key});
+  static const double containerWidth = 250;
+  static const double buttonHeigth = 45;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 4, 0, 25),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(
+                padding: const EdgeInsets.only(top: 50),
+                alignment: Alignment.center,
+                child: Image.asset(
+                  'assets/main_logo2.jpg',
+                  width: containerWidth,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: const EdgeInsets.only(top: 40),
+                width: containerWidth,
+                child: const Text(
+                  'Learn a language. Meet the word.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                margin: const EdgeInsets.fromLTRB(0, 100, 0, 30),
+                width: containerWidth,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+
+                      width: containerWidth,
+                      height: buttonHeigth,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 233,253,108),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                        ),
+                        onPressed: () {
+                          debugPrint('Received click');
+                        },
+                        child: const Text(
+                          'Get started',
+                          style: TextStyle(color: Colors.black, fontSize: 20),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: containerWidth,
+                      height: buttonHeigth,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 11,41,57),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                        ),
+                        onPressed: () {
+                          debugPrint('Received click');
+                        },
+                        child: const Text(
+                          'I have an account',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,169 +1,23 @@
+import 'package:demo/Screen/GettingStartedScreen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
   @override
   Widget build(BuildContext context) {
+    
+    const appTitle = 'Memrise';
     return MaterialApp(
-      title: 'Week 2 Demo Group 9',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Week 2 Demo Group 9'),
-        ),
-        body: const Center(
-          child: DemoStatefulWidget(),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: appTitle,
+     
+      home: GettingStarted(), 
     );
   }
 }
 
-class DemoStatefulWidget extends StatefulWidget {
-  const DemoStatefulWidget({super.key});
-
-  @override
-  State<DemoStatefulWidget> createState() => _DemoStatefulWidgetState();
-}
-
-class _DemoStatefulWidgetState extends State<DemoStatefulWidget> {
-  bool checked = true;
-  double sliderValue = 0.0;
-
-  void changeChecked() {
-    setState(() {
-      checked = !checked;
-      sliderValue += 0.2;
-      sliderValue %= 1.0;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(padding: const EdgeInsets.all(32), children: [
-      const Divider(),
-      Container(
-          color: checked ? Colors.blueAccent : Colors.amberAccent,
-          child: FloatingActionButton(
-            onPressed: () {
-              changeChecked();
-            },
-            backgroundColor: checked ? Colors.amberAccent : Colors.blueAccent,
-          )),
-      const Divider(),
-      OutlinedButton(
-        onPressed: () {
-          changeChecked();
-        },
-        child: Text(
-          checked ? 'Click here!' : 'Click here again!',
-        ),
-      ),
-      const Divider(),
-      Checkbox(
-        value: checked,
-        onChanged: (value) {
-          changeChecked();
-        },
-      ),
-      const Divider(),
-      Slider(
-        value: sliderValue,
-        onChanged: (value) {
-          setState(() {
-            checked = !checked;
-            sliderValue = value;
-          });
-        },
-      ),
-      const Divider(),
-      Switch(
-        value: !checked,
-        onChanged: (value) {
-          changeChecked();
-        },
-      ),
-      const Divider(),
-      IconButton(
-        icon: checked
-            ? const Icon(Icons.airplanemode_off)
-            : const Icon(Icons.airplanemode_on),
-        iconSize: 40,
-        onPressed: () {
-          changeChecked();
-        },
-      ),
-      const Divider(),
-      TextField(
-        decoration: InputDecoration(
-          border: const OutlineInputBorder(),
-          labelText: checked ? 'Enter your name' : 'Enter your age',
-        ),
-      ),
-      const Divider(),
-      Container(
-          color: checked ? Colors.blueAccent : Colors.amberAccent,
-          child: FloatingActionButton(
-            onPressed: () {
-              changeChecked();
-            },
-            backgroundColor: checked ? Colors.amberAccent : Colors.blueAccent,
-          )),
-      const Divider(),
-      OutlinedButton(
-        onPressed: () {
-          changeChecked();
-        },
-        child: Text(
-          checked ? 'Click here!' : 'Click here again!',
-        ),
-      ),
-      const Divider(),
-      Checkbox(
-        value: checked,
-        onChanged: (value) {
-          changeChecked();
-        },
-      ),
-      const Divider(),
-      Slider(
-        value: sliderValue,
-        onChanged: (value) {
-          setState(() {
-            checked = !checked;
-            sliderValue = value;
-          });
-        },
-      ),
-      const Divider(),
-      Switch(
-        value: !checked,
-        onChanged: (value) {
-          changeChecked();
-        },
-      ),
-      const Divider(),
-      IconButton(
-        icon: checked
-            ? const Icon(Icons.airplanemode_off)
-            : const Icon(Icons.airplanemode_on),
-        iconSize: 40,
-        onPressed: () {
-          changeChecked();
-        },
-      ),
-      const Divider(),
-      TextField(
-        decoration: InputDecoration(
-          border: const OutlineInputBorder(),
-          labelText: checked ? 'Enter your name' : 'Enter your age',
-        ),
-      ),
-      const Divider(),
-    ]);
-  }
-}
