46c1011896ee3ebe5df4aebce910e19cd4223c36
duongoku <nguyenvubinhduong@gmail.com>
2022-09-29 23:39:53 +0700

    Add language selection screen

--- lib/Screen/LanguageSelectionScreen.dart
+++ lib/Screen/LanguageSelectionScreen.dart
@@ -1,18 +1,120 @@
 import 'package:flutter/material.dart';
+import 'package:demo/Colors/CustomPalette.dart';
 
-class LanguageSelectionWidget extends StatefulWidget {
-  const LanguageSelectionWidget({super.key});
+class LanguageSelectionScreen extends StatefulWidget {
+  const LanguageSelectionScreen({super.key});
 
   @override
-  State<LanguageSelectionWidget> createState() =>
-      _LanguageSelectionWidgetState();
+  State<LanguageSelectionScreen> createState() =>
+      _LanguageSelectionScreenState();
 }
 
-class _LanguageSelectionWidgetState extends State<LanguageSelectionWidget> {
+class _LanguageSelectionScreenState extends State<LanguageSelectionScreen> {
+  static const languages = [
+    "English",
+    "Spanish",
+    "French",
+    "German",
+    "Italian",
+    "Portuguese",
+    "Russian",
+    "Turkish",
+    "Chinese",
+    "Japanese",
+    "Korean",
+    "Arabic",
+    "Hindi",
+    "Indonesian",
+    "Thai",
+    "Vietnamese",
+    "Dutch",
+    "Polish",
+    "Swedish",
+    "Danish",
+    "Norwegian",
+  ];
+
+  String selectedLanguage = languages[0];
+
+  TextStyle whiteText = const TextStyle(color: Colors.white, fontSize: 20);
+  TextStyle blackText =
+      const TextStyle(color: Color.fromRGBO(0, 0, 0, 1), fontSize: 20);
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.grey,
+    return Scaffold(
+      backgroundColor: CustomPalette.primaryColor,
+      body: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 200,
+              child: Center(
+                child: Text(
+                  "I speak",
+                  style: whiteText,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 200,
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: CustomPalette.secondaryColor,
+                ),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton(
+                      isExpanded: true,
+                      alignment: AlignmentDirectional.center,
+                      iconSize: 0.0,
+                      dropdownColor: CustomPalette.secondaryColor,
+                      items: languages.map((String language) {
+                        return DropdownMenuItem(
+                          value: language,
+                          child: Center(
+                            child: Text(
+                              language,
+                              style: blackText,
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (String? value) {
+                        setState(() {
+                          selectedLanguage = value!;
+                        });
+                      },
+                      value: selectedLanguage),
+                ),
+              ),
+            ),
+          ],
+        ),
+        Text(
+          "I want to learn",
+          style: whiteText,
+        ),
+        SizedBox(
+          height: 500.0,
+          width: 300.0,
+          child: DecoratedBox(
+            decoration: const BoxDecoration(
+                color: CustomPalette.lighterPrimaryColor,
+                borderRadius: BorderRadius.all(Radius.circular(15))),
+            child: ListView(
+              children: languages.map((String language) {
+                return ListTile(
+                  title: Center(
+                    child: Text(language, style: whiteText),
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
