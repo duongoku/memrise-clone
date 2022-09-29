bc2badc91d76442a1e0285535fddcc9be4cd70f3
duongoku <nguyenvubinhduong@gmail.com>
2022-09-29 20:38:16 +0700

    Added custom color palette

---
+++ lib/Colors/CustomPalette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomPalette {
+  CustomPalette._();
+
+  static const Color primaryColor = Color.fromARGB(255, 4, 0, 25);
+  static const Color lighterPrimaryColor = Color.fromARGB(255, 11, 41, 57);
+  static const Color secondaryColor = Color.fromARGB(255, 233, 253, 108);
+}
