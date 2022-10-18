234b0b72126ad3600901ae6c6ccba014e8eabb6e
duongoku <nguyenvubinhduong@gmail.com>
2022-10-18 17:05:43 +0700

    Add the first test

---
+++ test/first_test.dart
@@ -0,0 +1,29 @@
+import 'package:demo/main.dart';
+import 'package:demo/screens/getting_started_screen.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Test getting started screen #1', (tester) async {
+    startApp();
+    Widget testWidget = const MediaQuery(
+      data: MediaQueryData(),
+      child: MaterialApp(home: GettingStarted()),
+    );
+    tester.binding.window.physicalSizeTestValue = const Size(1800, 3200);
+    await tester.pumpWidget(testWidget);
+    await tester.tap(find.byType(ElevatedButton).first);
+  });
+  
+  testWidgets('Test getting started screen #2', (tester) async {
+    startApp();
+    Widget testWidget = const MediaQuery(
+      data: MediaQueryData(),
+      child: MaterialApp(home: GettingStarted()),
+    );
+    tester.binding.window.physicalSizeTestValue = const Size(1800, 3200);
+    await tester.pumpWidget(testWidget);
+    await tester.tap(find.byType(ElevatedButton).last);
+  });
+}
